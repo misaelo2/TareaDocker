ae7157ad23eeb01c989fdecf478a461bbbb4bb4f
Misael <misaelnodaquintero@gmail.com>
2018-01-28 13:48:47 +0100

    tarea4

--- dnginx.sh
+++ dnginx.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get install -y nginx
 
-echo "contenedor creado mediante variables"  > $DOCUMENTROOT/index.html
+echo "Este fichero ha sido modificado en un equipo sin docker y subido a github"  > $DOCUMENTROOT/index.html
 
 nginx -g "daemon off;"
 
